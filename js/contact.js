685e0b0927f8683fbcafbcf8b999deb701e4e44a
harllogeen <harllogeen2020@gmail.com>
2022-03-25 16:30:31 +0100

    implement toggling for small screen laptop

--- js/contact.js
+++ js/contact.js
@@ -5,46 +5,65 @@ document.getElementById('close-icon').addEventListener('click', () => {
   document.getElementById('nav').classList.add('ul-width')
 })
 
+// Form Validation
+const errText = document.querySelector('.err-hint')
+const errBorder = document.querySelector('.err-border')
+
 // CONTACT PAGE - SESSION STORAGE
-document.querySelector('.form').addEventListener('click', (e) => {
+document.querySelector('.form').addEventListener('submit', (e) => {
   e.preventDefault()
   let inputEls = document.querySelectorAll('input')
+  let textAreaEl = document.querySelector('textarea')
   let textObj
   let emailObj
   let textAreaObj
   let arr = []
   let inputObjects
   inputEls = [...inputEls]
-  const textAreaEl = document.querySelector('textarea')
   const inputs = () => {
     inputEls.map((input) => {
+      document.addEventListener('input', () => {
+        input.classList.remove('err-border')
+        input.nextElementSibling.classList.add('err-hide')
+      })
+      document.addEventListener('onfocus', () => {
+        textAreaEl.classList.remove('err-border')
+        textAreaEl.nextElementSibling.classList.add('err-hide')
+      })
       let inputValue = input.value
       let txtAreaValue = textAreaEl.value
-      console.log(inputValue, txtAreaValue)
       if (input.type === 'text' && inputValue) {
         textObj = {
           text: inputValue,
         }
+        console.log(input)
+      } else {
+        input.classList.add('err-border')
+        input.nextElementSibling.classList.remove('err-hide')
       }
       if (input.type === 'email' && inputValue) {
         emailObj = {
           email: inputValue,
         }
+      } else {
+        input.classList.add('err-border')
+        input.nextElementSibling.classList.remove('err-hide')
       }
-      if (txtAreaValue) {
+      if (inputValue && txtAreaValue) {
         textAreaObj = {
           textArea: txtAreaValue,
         }
+      } else {
+        textAreaEl.classList.add('err-border')
+        textAreaEl.nextElementSibling.classList.remove('err-hide')
       }
       inputObjects = { ...emailObj, ...textObj, ...textAreaObj }
 
       // console.log(inputObjects)
     })
     arr.push(inputObjects)
-    console.log(arr)
     sessionStorage.setItem('contactDetails', JSON.stringify(arr))
     let form = JSON.parse(localStorage.getItem('contactDetails'))
-    console.log(form)
   }
   inputs()
   // console.log(textAreaEl)
